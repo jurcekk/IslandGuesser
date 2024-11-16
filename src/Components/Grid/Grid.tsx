d22633f537364a7d1739fa14789582f2a5a9a601
David Jurcek <jurcekdavid@gmail.com>
2024-11-16 17:07:42 +0100

    Added qa part of the challenge and fixed some of the warning

--- src/Components/Grid/Grid.tsx
+++ src/Components/Grid/Grid.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from 'react';
 import useGrid from '../../hooks/useGrid';
 import Loader from '../Loader';
